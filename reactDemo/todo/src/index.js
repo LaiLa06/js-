6c2d3602654a1e67f7533f2e685afe5a0bfab531
Laila06 <sias_xg_121@126.com>
2018-06-27 10:11:57 +0800

    1

--- reactDemo/todo/src/index.js
+++ reactDemo/todo/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Vote from "./component/vote/Vote";
 import 'bootstrap/dist/css/bootstrap.css'
 import store from './store'
+import './static/less/todo.less'
 
-ReactDOM.render(<main>
-    <Vote title={'英格兰VS巴拿马'} store={store}/>
+
+ReactDOM.render(<main className={'panel panel-default'}>
+
 </main>, document.getElementById('root'));
